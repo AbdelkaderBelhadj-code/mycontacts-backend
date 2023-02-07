0950e275dafe1be19012e4dd3df319abd2a22521
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-02-07 16:49:54 +0100

    test1

--- server.js
+++ server.js
@@ -8,13 +8,12 @@ const app = express();
 
 const port = process.env.PORT || 5000 ; 
 
-
 app.use(express.json());
 app.use(errorHandler);
 app.use("/api/contacts" , require("./routes/contactRoutes"));
 app.use("/api/users" , require("./routes/userRoutes"));
 
-
+    
 connectDb();
 
 app.listen(port , () => {
